f3c75c08e644a1733723de75cb494a81dcd28f56
Bryan White <bryanchriswhite@gmail.com>
2019-01-05 09:25:49 +0100

    refactor & multi-user support

---
+++ defaults.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+renamed_color="\e[35m"
+ahead_color="\e[34m"
+new_color="\e[32m"
+untracked_color=""
+deleted_color="\e[37m"
+dirty_color="\e[31m"
--- install.sh
+++ install.sh
@@ -5,57 +5,65 @@ dir=$(dirname $(realpath $0))
 files=".gitconfig
 .vimrc
 .tmux.conf"
-bashrc="$HOME/.bashrc"
+bashrc="${HOME}/.bashrc"
 bash_files=".bash_aliases
 .bash_functions
 .bash_ps1"
 to_link=("${files[@]}" "${bash_files[@]}")
 
 for f in $bash_files; do
-  if ! grep -qP '(?<!#)(source|\.)\s+.*'$f $bashrc; then
-    cat >> $bashrc << EOF
-if [ -f ~/$f ]; then
-  . ~/$f
+  if ! grep -qP '(?<!#)(source|\.)\s+.*'${f} ${bashrc}; then
+    cat >> ${bashrc} << EOF
+if [ -f ~/${f} ]; then
+  . ~/${f}
 fi
 EOF
   fi
 done
 
-cd $dir && git submodule update --init
+cd ${dir} && git submodule update --init
 
 # Vim plugins
-vim_autoload=$HOME/.vim/autoload
-vim_bundle=$HOME/.vim/bundle
-vim_pathogen=$vim_autoload/pathogen.vim
-vim_nerdtree=$vim_bundle/nerdtree
-if [ ! -d $vim_autoload ]; then
-  mkdir -p $vim_autoload
+vim_autoload=${HOME}/.vim/autoload
+vim_bundle=${HOME}/.vim/bundle
+vim_pathogen=${vim_autoload}/pathogen.vim
+vim_nerdtree=${vim_bundle}/nerdtree
+if [[ ! -d ${vim_autoload} ]]; then
+  mkdir -p ${vim_autoload}
 fi
 
-if [ ! -d $vim_bundle ]; then
-  mkdir -p $vim_bundle
+if [[ ! -d ${vim_bundle} ]]; then
+  mkdir -p ${vim_bundle}
 fi
 
-if [ ! -e $vim_pathogen ]; then
+if [[ ! -e ${vim_pathogen} ]]; then
   echo "Creating vim pathogen symlink";
-  ln -s "$dir/vim/vim-pathogen/autoload/pathogen.vim" $vim_pathogen;
+  ln -s "${dir}/vim/vim-pathogen/autoload/pathogen.vim" ${vim_pathogen};
 fi
 
-if [ ! -e $vim_nerdtree ]; then
+if [[ ! -e ${vim_nerdtree} ]]; then
   echo "Creating vim nerdtree symlink";
-  ln -s "$dir/vim/nerdtree" $vim_nerdtree;
+  ln -s "${dir}/vim/nerdtree" ${vim_nerdtree};
 fi
 
 # Symlink dotfiles
 for f in ${to_link[@]}; do
-  target_path="$dir/$f";
-  link_path="$HOME/$f";
-
-  if [ -f $link_path ]; then
-    echo "File $link_path exists and is not a symlink; NOT OVERWRITING!";
+  user=$(whoami)
+  user_target=${dir}/${f}.${user}
+  if [[ -e ${user_target} ]]; then
+    echo "user target"
+    target_path="${user_target}";
   else
-    echo "creating symlink from $target_path to $link_path"
-    ln -s $target_path $link_path;
+    echo "generic target"
+    target_path="${dir}/${f}";
+  fi
+  link_path="${HOME}/${f}";
+
+  if [[ -f ${link_path} ]]; then
+    echo "File ${link_path} exists and is not a symlink; NOT OVERWRITING!";
+  else
+    echo "creating symlink from ${target_path} to ${link_path}"
+    ln -s ${target_path} ${link_path};
   fi
 
 done
